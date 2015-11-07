14835b89900aac8fb66a9a14eba3cfe5acc9fff4
mario.filino <mario.filino@gmail.com>
2015-11-07 14:16:04 +0700

    add example dataset

--- src/main/java/weka/custom_classifier/PerceptronTrainingRule.java
+++ src/main/java/weka/custom_classifier/PerceptronTrainingRule.java
@@ -1,5 +1,18 @@
 package weka.custom_classifier;
 
-public class PerceptronTrainingRule {
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+import weka.core.OptionHandler;
+import weka.core.Randomizable;
+import weka.core.WeightedInstancesHandler;
 
+public class PerceptronTrainingRule extends Classifier {
+
+	@Override
+	public void buildClassifier(Instances data) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
